97a52facc262c2fa28d821ad7cc7908d3a555a15
andrew <apologboy@gmail.com>
2016-08-05 16:31:17 +0500

    Edits

---
+++ src/Entity/Article.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Andrew45105\SFC\Entity;
+
+/**
+ * Describes article entity
+ * 
+ * Class Article
+ * 
+ * @package Andrew45105\SFC\Entity
+ */
+class Article
+{
+    /**
+     * @var $id
+     */
+    protected $id;
+
+    /**
+     * @var $createdAt
+     */
+    protected $createdAt;
+
+    /**
+     * @var $title
+     */
+    protected $title;
+
+    /**
+     * @var $text
+     */
+    protected $text;
+    
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param int $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+    
+}
--- tests/Controller/IndexControllerTest.php
+++ tests/Controller/IndexControllerTest.php
@@ -1,9 +1,31 @@
 <?php
 
+require_once __DIR__.'/../../vendor/autoload.php';
+
 /**
+ * Contains methods for test IndexController class
+ *
  * Class IndexControllerTest
  */
-class IndexControllerTest
+class IndexControllerTest extends PHPUnit_Framework_TestCase
 {
+
+    public function testIndexAction()
+    {
+
+        $paramsContainer = new \Andrew45105\SFC\Container\ParamsContainer(__DIR__.'/../../app/config');
+        $siteUrl = $paramsContainer->getParam('site_url');
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $siteUrl);
+
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        $this->assertContains('Its index page', $data);
+    }
     
 }
